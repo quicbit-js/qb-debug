73dbd72ac96d8db330bb9006c6895365f1d168ea
mvoss9000 <mvoss9000@yahoo.com>
2019-01-08 02:58:08 -0500

    put buf2str in separate module

--- index.js
+++ index.js
@@ -15,76 +15,7 @@
 // OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
 var debug = require('debug')
-
-// array lookup of printable ascii chars
-var ASCII = (function () {
-  var ret = new Uint8Array(256).fill(0)
-  for (var i=32; i <= 127; i++) {
-    ret[i] = 1
-  }
-  return ret
-})()
-
-// return 'buf', 'arr', 'view', or null (not an array type)
-function arr_type (v) {
-  if (Buffer.isBuffer(v)) {
-    return 'buf'
-  } else if (Array.isArray(v)) {
-    return 'arr'
-  } else if (ArrayBuffer.isView(v)) {
-    return 'view'
-  } else {
-    return null
-  }
-}
-
-// return 'ascii', 'hex', or null (for objects or arrays with non-byte values)s
-function arr_format (v) {
-  if (v.type === 'Buffer' && v.data) {
-    v = v.data
-  }
-  switch (arr_type(v)) {
-    case 'arr':
-      // check that values are bytes
-      for (var i=0; i < v.length; i++) {
-        if (typeof v[i] !== 'number' || v[i] < 0 || v[i] > 0xFF) {
-          return null
-        }
-      }
-      break
-    case 'buf': case 'view':
-      break
-    default:
-      return null
-  }
-
-  // check if all values are ascii
-  for (var j=0; j<v.length; j++) {
-    if (ASCII[v[j]] === 0) {
-      return 'hex'
-    }
-  }
-  return 'ascii'
-}
-
-function buf2str (v, maxchars) {
-  var ret
-  switch (arr_format(v)) {
-    case 'ascii':
-      ret = Buffer.from(v).slice(0, maxchars).toString('ascii')
-      if (v.length > maxchars) {
-        ret += '..'
-      }
-      break
-    case 'hex':
-      // 1 byte = 2 chars.
-      ret = 'x' + Buffer.from(v).slice(0, maxchars/2).toString('hex').toUpperCase()
-      break
-    default:
-      ret = v   // not a byte array.  do not convert.
-  }
-  return ret
-}
+var buf2str = require('qb-buf2str')
 
 // format a buffer to a truncated hex string of maxchars in length.
 // format other objects to json applying buffer conversion
@@ -92,16 +23,13 @@ function format_x_fn (maxchars) {
   return function format_x (v) {
     if (v && typeof v === 'object') {
       v = buf2str(v, maxchars)
-      if (typeof v === 'string') { return v }
-
-      return JSON.stringify(v, function (k, v) {
-        if (v && typeof v === 'object') {
-          v = buf2str(v, maxchars)
-        }
-        return v
-      })
+      if (typeof v !== 'string') {
+        v = buf2str.stringify(v)
+      }
+    } else {
+      v = String(v)
     }
-    return String(v)
+    return v
   }
 }
 
